ccfb01df4401abf58d711c8faa16298075dee747
feranmiba <amiolademilade@gmail.com>
2024-09-04 09:45:05 +0100

    added-functionality

--- frontend/views/Livestream/Archive/Archive.tsx
+++ frontend/views/Livestream/Archive/Archive.tsx
@@ -1,58 +1,105 @@
-import React from 'react'
-import Image from 'next/image'
+import React, { useState, useEffect } from 'react'
+import Image, { StaticImageData } from 'next/image'
 import Logo from "../../../public/resourcLogo.png"
 import Link from 'next/link'
 import Archives from '../../../public/archive.png'
 import Bgsmall from '../../../public/bgsmall.png'
 
 
-const Archiveses = [
+
+type MockArchive = {
+    id: number,
+    vid: StaticImageData,
+    title: string,
+    speaker: string,
+    time: string,
+    type: string,
+    date: string
+}
+
+
+const ArchivesesData: MockArchive[] = [
     {
         id:0 ,
         vid: Archives,
         title: "Field Notes from the Trenches: Ensuring High-Availability and Resilience in Blockchain Node Operations",
-        speaker: "by: Ukeme David Eseme"
+        speaker: "by: Ukeme David Eseme", 
+        time: "8:00Am",
+        type: "panel Session",
+        date: "17/10/19"
     },
     {
         id: 1 ,
         vid: Archives,
-        title: "Field Notes from the Trenches: Ensuring High-Availability and Resilience in Blockchain Node Operations",
-        speaker: "by: Ukeme David Eseme"
+        title: "How to write content that connects with target audience",
+        speaker: "by: Victor Fawole",
+        time: "9:00am",
+        type: "panel Session",
+        date: "17/10/19"
     },
     {
         id:2,
         vid: Archives,
-        title: "Field Notes from the Trenches: Ensuring High-Availability and Resilience in Blockchain Node Operations",
-        speaker: "by: Ukeme David Eseme"
+        title: "Science on the blockchain - Introduction to Decentralized Science",
+        speaker: "by: Scott Itepu",
+        time: "10:00Am",
+        type: "panel Session",
+        date: "17/10/19"
     },
     {
         id:3,
         vid: Archives,
         title: "Field Notes from the Trenches: Ensuring High-Availability and Resilience in Blockchain Node Operations",
-        speaker: "by: Ukeme David Eseme"
+        speaker: "by: Ukeme David Eseme",
+        time: "12:00PM",
+        type: "panel Session",
+        date: "17/10/19"
     },
     {
         id:4,
         vid: Archives,
         title: "Field Notes from the Trenches: Ensuring High-Availability and Resilience in Blockchain Node Operations",
-        speaker: "by: Ukeme David Eseme"
+        speaker: "by: Ukeme David Eseme",
+        time: "8:00",
+        type: "panel Session",
+        date: "17/10/19"
     },
     {
         id:5,
         vid: Archives,
         title: "Field Notes from the Trenches: Ensuring High-Availability and Resilience in Blockchain Node Operations",
-        speaker: "by: Ukeme David Eseme"
+        speaker: "by: Ukeme David Eseme",
+        time: "8:00",
+        type: "panel Session",
+        date: "17/10/19"
     },
 ]
 
 function Archive() {
+    const [ selectedArchive, setSelectedArchive ] = useState<MockArchive | null>(null);
+    const [ VidArch, setVidArchive ] =  useState<MockArchive[]>([]);
+
+    useEffect(() => {
+        // Simulate API call
+        const fetchData = async () => {
+            // const response = await fetch('/api/archives');
+            // const data = await response.json();
+            const data = ArchivesesData; // using mockData for now
+            setVidArchive(data);
+            setSelectedArchive(data[0]); // default to the first item
+        };
+
+        fetchData();
+    }, []);
+
+
+
   return (
     <div className='px-3 lg:px-16 xl:px-56'>
 
     <header className="flex flex-col md:flex-row justify-between items-center w-full py-10">
         <div className="flex items-center gap-2">
-            <Link href="/">
-           
+            <Link href="/" className='flex space-x-3 items-center'>
                     <Image src={Logo} alt="" width={0} height={0} className="w-[50px] md:w-[100px]" />
                     <p className='font-semibold text-xl md:text-2xl'>Web3lagos Event</p>
             </Link>
@@ -74,22 +121,46 @@ function Archive() {
                 <p>Main Event</p>
             </div>
             <div className='mt-4'>
-                <Image src={Archives} alt='ghghgh' layout='responsive' width={1000} height={500} />
+                <Image src={selectedArchive?.vid || Archives} alt='ghghgh' layout='responsive' width={1000} height={500} />
             </div>
             <div className='space-y-8 mt-5 text-start flex flex-col items-start'>
                 <p className='border px-3 py-2 rounded-xl bg-[#EFF4FF] font-semibold'>Detail</p>
                 <p className='font-medium text-2xl md:text-4xl leading-8 md:leading-10'>Field Notes from the Trenches: Ensuring High-Availability and Resilience in Blockchain Node Operations</p>
             </div>
             <div className='flex flex-wrap gap-4 mt-5'>
-                {['Speaker', 'Speaker', 'Speaker', 'Speaker'].map((text, index) => (
-                    <div key={index} className='flex items-center'>
+               
+                    <div className='flex items-center'>
                         <div className='h-12 w-1 bg-[#A02B2D] mx-4' />
                         <div>
-                            <p className='font-medium'>{text}</p>
-                            <p>Victor Fawale</p>
+                        <p className='font-medium'>Speaker</p>
+                        <p>{selectedArchive?.speaker}</p>
                         </div>
                     </div>
-                ))}
+
+                    <div className='flex items-center'>
+                        <div className='h-12 w-1 bg-[#A02B2D] mx-4' />
+                        <div>
+                        <p className='font-medium'>Type</p>
+                        <p>{selectedArchive?.type}</p>
+                        </div>
+                    </div>
+
+                    <div className='flex items-center'>
+                        <div className='h-12 w-1 bg-[#A02B2D] mx-4' />
+                        <div>
+                            <p className='font-medium'>Date</p>
+                            <p>{selectedArchive?.date}</p>
+                        </div>
+                    </div>
+
+                    <div className='flex items-center'>
+                        <div className='h-12 w-1 bg-[#A02B2D] mx-4' />
+                        <div>
+                            <p className='font-medium'>Time</p>
+                            <p>{selectedArchive?.time}</p>
+                        </div>
+                    </div>
+               
             </div>
         </div>
 
@@ -100,15 +171,15 @@ function Archive() {
             </div>
             <div className='relative overflow-y-auto h-[70vh]' style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
                 <div className='flex flex-col gap-10 mt-5 overflow-y-auto pr-5'>
-                    {Archiveses.map((arch) => (
-                        <div key={arch.id} className='flex gap-10 flex-wrap lg:flex-nowrap'>
+                    {VidArch.map((arch) => (
+                        <div key={arch.id} className='flex gap-10 flex-wrap lg:flex-nowrap'  onClick={() => setSelectedArchive(arch)}>
                             <div className='lg:w-[40%]'>
                                 <Image src={arch.vid} alt='hghsd' className='rounded-3xl'   />
                             </div>
                             <div className='space-y-4 mt-3'>
                                 <h1 className='font-semibold'>
-                                    {arch.title.split(' ').slice(0, 10).join(' ')}
-                                    {arch.title.split(' ').length > 10 && '...'}
+                                    {arch.title.split(' ').slice(0, 6).join(' ')}
+                                    {arch.title.split(' ').length > 7 && '...'}
                                 </h1>
                                 <p className='text-[#545454]'>{arch.speaker}</p>
                             </div>
